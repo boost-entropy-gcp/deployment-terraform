3970952de4bb3fbaeb1ec2bddb070afb76eb503d
Luighi <luighiavz@gmail.com>
2021-02-22 00:27:57 -0500

    Changing format for key

--- linode/keypair.tf
+++ linode/keypair.tf
@@ -5,6 +5,6 @@ resource "tls_private_key" "key" {
 
 resource "linode_sshkey" "key" {
   label      = "key"
-  ssh_key    = tls_private_key.key.public_key_openssh
+  ssh_key    = tls_private_key.key.public_key_pem
   depends_on = [tls_private_key.key]
 }
