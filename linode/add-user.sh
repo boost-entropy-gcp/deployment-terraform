b317fca3629d91a525285a41281c233f99240747
Luighi <luighiavz@gmail.com>
2021-02-24 16:06:43 -0500

    Adding default user script

---
+++ linode/add-user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+	echo "Not username provided, using user"
+	USERNAME=user
+else
+	USERNAME=$1
+fi
+
+useradd $USERNAME --create-home --user-group --shell /bin/bash --groups sudo
+
+USER_HOME=/home/$USERNAME
+
+mkdir -p $USER_HOME/.ssh
+cp /root/.ssh/authorized_keys $USER_HOME/.ssh/
+chown $USERNAME:$USERNAME $USER_HOME/.ssh/authorized_keys
+
+cat << EOF > /etc/sudoers.d/90-cloud-init-users 
+# User rules for $USERNAME
+$USERNAME ALL=(ALL) NOPASSWD:ALL
+EOF
