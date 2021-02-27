bad70f88b9584dad78804f598daeb237c4a2c62b
Luighi <luighiavz@gmail.com>
2021-02-26 20:50:17 -0500

    Delete template file

--- linode/render_files.tf
+++
@@ -1,4 +0,0 @@
-data "template_file" "cloud_init" {
-  template = file("${path.module}/add-user.sh")
-
-}
--- linode/stackscript.tf
+++ linode/stackscript.tf
@@ -1,7 +1,7 @@
 resource "linode_stackscript" "add_user" {
   label       = "user"
   description = "Add init user"
-  script      = data.template_file.could_init.rendered
+  script      = file("${path.module}/add-user.sh")
   images      = [var.linode_images["ubuntu20"], var.linode_images["ubuntu18"]]
   rev_note    = "initial version"
 }
